4cfadf05ce6c50a428a43a82a5491a98639343b0
João Victor <joaosantos.senai564@gmail.com>
2023-11-13 10:46:32 -0300

    git ignore / delete

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -20,5 +20,26 @@ studentsRoutes.post("/", (req, res) => {
     return res.status(200).send({ message: `Create student with name ${name}, email ${email}, age ${age}`});
 });
 
+studentsRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, email, age } = req.body
+
+    if(!name || !email || !age){
+        return res.status(400).send({ message: "Missing required informations",});
+    }
+    return res.status(200).send({ message: `Update student with id ${id}, witch name ${name}, email ${email}, age ${age}`});
+});
+
+studentsRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    return res.status(200).send({ message: `Delete student witch id ${id}`});
+});
+
+ 
+
+
+
+
 
 export default studentsRoutes;
