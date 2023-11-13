c3b16d28cb4d71d5019469c245c5d02cbed64c99
João Victor <joaosantos.senai564@gmail.com>
2023-11-13 10:28:15 -0300

    first commit

---
+++ src/routes/index.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import studentsRoutes from './students.routes.js';
+
+const rotas = Router();
+
+rotas.use("/students", studentsRoutes);
+
+rotas.get("/", (req, res) => {
+    return res.status(200).send(
+        { message: "Servidor blz!"}
+    );
+});
+
+export default rotas;
